1bd8b3a81ab2774438cbeaae1ceaa9ca1d50f388
sonumittal1888-byte <sonumittal1888@gmail.com>
2026-05-04 09:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,37 +7,34 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Sabse zaroori line: files dhoondne ke liye
-app.use(express.static(__dirname));
+// Ye server ko batayega ki design 'public' folder mein hai
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Database Connection
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("DB Connected"))
-  .catch(err => console.log(err));
+  .then(() => console.log("DB Connected Successfully"))
+  .catch(err => console.log("DB Connection Error:", err));
 
 const User = mongoose.model('User', new mongoose.Schema({
   email: { type: String, unique: true },
   password: String
 }));
 
-// Home page par seedha index.html dikhane ke liye
+// Home page par design dikhao
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Sign Up Route
 app.post('/signup', async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
     res.status(201).send({ message: "Account Ban Gaya!" });
-  } catch (e) { res.status(400).send({ error: "Email pehle se hai!" }); }
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-  if (user) res.send({ message: "Welcome Back!" });
-  else res.status(400).send({ error: "Galat Details!" });
+  } catch (e) { 
+    res.status(400).send({ error: "Email pehle se hai!" }); 
+  }
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running on " + PORT));
